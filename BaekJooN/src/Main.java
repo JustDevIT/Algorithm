ee3ca6e128a2eb6f7579fbec50b9c1be5f180fea
godokan <godokan@DESKTOP-UMPDF02.mshome.net>
2018-08-16 12:50:09 +0900

    2018.8.16

--- BaekJooN/src/Main.java
+++ BaekJooN/src/Main.java
@@ -1,9 +1,52 @@
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void bfs() {
+		
 	}
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		//System.setIn(new FileInputStream("D://Project/Algorithm/Algorithm/BaekJooN/TestCase/Sample.tx"));
+		Scanner sc = new Scanner(System.in);
+		
+		int mod = 1000000;
+		int d[][] = new int[1001][3];
+
+		int n=sc.nextInt();
+		
+		d[0][0] =1;
+		
+	    for (int i=1; i<=n; i++) {
+	        d[i][0] = d[i-1][0] + d[i-1][1] + d[i-1][2];
+	        d[i][1] = d[i-1][0];
+	        d[i][2] = d[i-1][1];
+	        
+	        for (int j=0; j<3; j++) {
+	            d[i][j] %= mod;
+	       }
+	        
+	    }
+	    
+	    int ans = 0;
+	    
+	    for (int i=0; i<3; i++) {
+	        ans += d[n][i];
+	        ans %= mod;
+	    }
+	    
+	    for (int i=1; i<=n; i++) {
+	        for (int j=0; j<3; j++) {
+	            for (int k=0; k<3; k++) {
+	                long temp = (long)d[i-1][j] * d[n-i][k];
+	                ans += temp % mod;
+	                ans %= mod;
+	            }
+	        }
+	    }
+		    
+		    System.out.println(ans);
+		}
 
 }
