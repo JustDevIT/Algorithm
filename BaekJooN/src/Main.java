a74003f55caab063da94fce3a8322ba836d6528e
godokan <godokan@DESKTOP-UMPDF02.mshome.net>
2018-08-26 14:09:36 +0900

    수정완료

--- BaekJooN/src/Main.java
+++ BaekJooN/src/Main.java
@@ -1,52 +1,52 @@
+import java.io.FileInputStream;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
 
-	public void bfs() {
+	static int[][] map = new int[1001][1001];
+	static boolean[] visited;
+	
+	static void dfs(int c) {
+		// 1 2
+		// 2 4
 		
+		visited[c] = true;
+		System.out.print(c) ;
+		
+		for(int i=1; i< visited.length; i++) {
+			if(map[c][i]==1 && !visited[i]) {
+				
+				dfs(i);
+			}
+		}
 	}
 	
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		//System.setIn(new FileInputStream("D://Project/Algorithm/Algorithm/BaekJooN/TestCase/Sample.tx"));
+		System.setIn(new FileInputStream("D://Project/Algorithm/Algorithm/BaekJooN/TestCase/Sample.txt"));
 		Scanner sc = new Scanner(System.in);
 		
-		int mod = 1000000;
-		int d[][] = new int[1001][3];
-
-		int n=sc.nextInt();
+		// 4 5 1
+		int n = sc.nextInt();
+		int s = sc.nextInt();
+		int c = sc.nextInt();
 		
-		d[0][0] =1;
+		visited = new boolean[s+1];
 		
-	    for (int i=1; i<=n; i++) {
-	        d[i][0] = d[i-1][0] + d[i-1][1] + d[i-1][2];
-	        d[i][1] = d[i-1][0];
-	        d[i][2] = d[i-1][1];
-	        
-	        for (int j=0; j<3; j++) {
-	            d[i][j] %= mod;
-	       }
-	        
-	    }
-	    
-	    int ans = 0;
-	    
-	    for (int i=0; i<3; i++) {
-	        ans += d[n][i];
-	        ans %= mod;
-	    }
-	    
-	    for (int i=1; i<=n; i++) {
-	        for (int j=0; j<3; j++) {
-	            for (int k=0; k<3; k++) {
-	                long temp = (long)d[i-1][j] * d[n-i][k];
-	                ans += temp % mod;
-	                ans %= mod;
-	            }
-	        }
-	    }
-		    
-		    System.out.println(ans);
+		for(int i=0; i<visited.length; i++) {
+			visited[i] = false;
 		}
+		
+		for(int i =0;i<s;i++) {
+			int s1 = sc.nextInt();
+			int t1 = sc.nextInt();
+			
+			map[s1][t1] = map[t1][s1] = 1;
+		}
+		
+		dfs(c);
+	}
+	
 
 }
