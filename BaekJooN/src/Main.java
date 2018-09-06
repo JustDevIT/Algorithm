562d5af9a32c2b31c5aafb519c6186984a65c16e
godokan <godokan@DESKTOP-UMPDF02>
2018-09-06 16:21:00 +0900

    이천수 추가

--- BaekJooN/src/Main.java
+++ BaekJooN/src/Main.java
@@ -1,45 +1,33 @@
 import java.io.FileInputStream;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-
-	static int[][][] dp= new int[101][100][2];
- 
+	
+	static void tile1(int n) {
+		long[] k = new long[1000000];
+		
+		k[0] = k[1] = 1;
+		
+		for(int i = 2; i<=n; i++) {
+			k[i] = k[i-2] + k[i-1];
+			k[i] %= 15746;
+		}
+		
+		System.out.print(k[n]);
+		
+	}
+	
 		
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
+		
 		System.setIn(new FileInputStream("D://Project/Algorithm/Algorithm/BaekJooN/TestCase/Sample.txt"));
 		Scanner sc = new Scanner(System.in);
 		
+		int n = sc.nextInt();
 		
-		dp[1][0][1] = dp[1][0][0] = 1;
-	 
-	        
-	        for (int k = 0; k < 100; k++) {
-	        	 
-	            for (int n = 2; n <= 100; n++) {
-	                if (k == 0) {
-	                    dp[n][k][1] = dp[n - 1][k][0];
-	                } else {
-	                    dp[n][k][1] = dp[n - 1][k][0] + dp[n - 1][k - 1][1];
-	                }
-	                
-	                dp[n][k][0] = dp[n - 1][k][0] + dp[n - 1][k][1];
-	            }
-	     
-	        }
-
-	        int n = 3;
-	        int k = 1;
-	        System.out.println(dp[n][k][0] + dp[n][k][1]);
-	        
-	        n = 2;
-	        k = 1;
-	        System.out.println(dp[n][k][0] + dp[n][k][1]);
-
-
+		tile1(n);
+		
 	}
 	
 
