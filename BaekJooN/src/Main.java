1fbae46aac3741bfb730a8477f647a2d669992ce
godokan <godokan@DESKTOP-UMPDF02>
2018-09-06 10:03:37 +0900

    반영

--- BaekJooN/src/Main.java
+++ BaekJooN/src/Main.java
@@ -1,52 +1,47 @@
 import java.io.FileInputStream;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
 
-	static int[][] map = new int[1001][1001];
-	static boolean[] visited;
-	
-	static void dfs(int c) {
-		// 1 2
-		// 2 4
+	static int[][][] dp= new int[101][100][2];
+
 		
-		visited[c] = true;
-		System.out.print(c) ;
-		
-		for(int i=1; i< visited.length; i++) {
-			if(map[c][i]==1 && !visited[i]) {
-				
-				dfs(i);
-			}
-		}
-	}
-	
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		System.setIn(new FileInputStream("D://Project/Algorithm/Algorithm/BaekJooN/TestCase/Sample.txt"));
 		Scanner sc = new Scanner(System.in);
 		
-		// 4 5 1
-		int n = sc.nextInt();
-		int s = sc.nextInt();
-		int c = sc.nextInt();
 		
-		visited = new boolean[s+1];
-		
-		for(int i=0; i<visited.length; i++) {
-			visited[i] = false;
-		}
-		
-		for(int i =0;i<s;i++) {
-			int s1 = sc.nextInt();
-			int t1 = sc.nextInt();
-			
-			map[s1][t1] = map[t1][s1] = 1;
-		}
-		
-		dfs(c);
+		dp[1][0][1] = dp[1][0][0] = 1;
+	 
+	        
+	        for (int k = 0; k < 100; k++) {
+	        	 
+	            for (int n = 2; n <= 100; n++) {
+	                if (k == 0) {
+	                    dp[n][k][1] = dp[n - 1][k][0];
+	                } else {
+	                    dp[n][k][1] = dp[n - 1][k][0] + dp[n - 1][k - 1][1];
+	                }
+	                
+	                dp[n][k][0] = dp[n - 1][k][0] + dp[n - 1][k][1];
+	            }
+	     
+	        }
+
+	        int n = 3;
+	        int k = 1;
+	        System.out.println(dp[n][k][0] + dp[n][k][1]);
+	        
+	        n = 2;
+	        k = 1;
+	        System.out.println(dp[n][k][0] + dp[n][k][1]);
+
+
 	}
 	
 
 }
+
