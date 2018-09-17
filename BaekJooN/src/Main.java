08c17d6438fd3e3ac518c80ea1e6151273e6004e
starterfly <starterfly@gmail.com>
2018-09-18 00:06:32 +0900

    r

--- BaekJooN/src/Main.java
+++ BaekJooN/src/Main.java
@@ -3,49 +3,58 @@ import java.util.Scanner;
 
 public class Main {
 
+	static int m;
+	static int map[][];
+	static int k[][];
+	static int ret;
+	static int max;
+	
+	static void dfs(int day, int sum){
+	    
+		System.out.println(day + " " + sum + " ");
+		if(day == m+1){
+	    	ret = Math.max(ret, sum);
+	        return;
+	    }
+	    //상담을 안하는 경우
+	    dfs(day+1, sum);
+	    
+	    //상담을 할 수 있을 경우는 상담을 한다.
+	    if(day+map[day][1] <= m+1) {
+	        dfs(day+map[day][1], sum+map[day][2]);
+	    }
+	}
+	
 	
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		
-		System.setIn(new FileInputStream("D://Project/Algorithm/Algorithm/BaekJooN/TestCase/Sample.txt"));
-		//System.setIn(new FileInputStream("C://Users//영훈//git//Algorithm//BaekJooN//TestCase//Sample.txt"));
+		//System.setIn(new FileInputStream("D://Project/Algorithm/Algorithm/BaekJooN/TestCase/Sample.txt"));
+		System.setIn(new FileInputStream("C://Users//영훈//git//Algorithm//BaekJooN//TestCase//Sample.txt"));
 		Scanner sc = new Scanner(System.in);
-				
-		/*
-		 * 		1 2 3 4 5 6 7
-
-				d   1    2    3    4
-				t   3    5    1    
-				p   10  20   10
-				
-				
-				k[n] = k(d-t)
-				
-				
-				V[k] = 1일
-				
-				
-				1일 -> 4, 5 ,6 ,7
-				2일 -> x
-				3일 -> 4, 5, 6, 7
-				
-				d  1      2      3
-				t  3      5      1
-				p  10     20
-				
-				n = 현 위치
-				
-				k[n] = max(k[n-1] + timetable[n-d][2]), timetable[n][2] + timetable[n-t][2]!=null)
-				
-				work[0][0] = d
-				work[0][1] = t
-				work[0][2] = p
-				
-				1
-				3
-				
-				10
-		 */
 		
+		
+		m = sc.nextInt();
+		
+		map = new int[m+1][3];
+		k = new int[m+1][2];
+		
+		for(int i=1; i<=m;i++) {
+			map[i][0] = i; 
+			map[i][1] = sc.nextInt();
+			
+			int t = sc.nextInt();
+			
+			if((i-1) + map[i][1] > m) {
+				map[i][2] = 0;
+			}else {
+				map[i][2] = t;
+			}
+		}
+		
+		dfs(1, 0);
+		
+		System.out.println(ret);
+	
 	}
 }
