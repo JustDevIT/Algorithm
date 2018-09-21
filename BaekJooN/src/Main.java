84cd4e7181b2315330509c14b1ab65c467ab8079
starterfly <starterfly@gmail.com>
2018-09-22 00:40:32 +0900

    R

--- BaekJooN/src/Main.java
+++ BaekJooN/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.FileInputStream;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -7,37 +8,44 @@ public class Main {
 	
 	static int n, m;
 	static int k;
-	static int[][] s;
-	static int ret;
+	static int[] happy, need; 
+	static int[][] cache;
 	
-	static void solve(int i, int sumN, int sumM) {
+	static int solve(int capactiy, int tonosi, int item) {
 		
-		System.out.println(i);
-		if(i==k) {
-			
-			if(n <= sumN  && m >= sumN) {
-				
-				ret = Math.max(ret, sumM);
-			}
-			
-			return;
+		System.out.println(" 1 : " + item + " : " + capactiy + " : " + tonosi);
+		
+		if(item==k+1) {
+			return 0;
 		}
-
-		solve(i+1,sumN,sumM);
 		
-		if(sumN <= m)
+		int ret = cache[tonosi][item];
+		
+		if(ret != -1) {
+			return ret;
+		}
+		
+		// 과목 선택 안함
+		ret = solve(capactiy, tonosi, item+1);
+		
+		cache[tonosi][item] = ret;
+		System.out.println(" 2 : " + item + " : " + capactiy + " : " + tonosi);
+		
+		// 과목 선택한 경우
+		if(capactiy <= m)
 		{
- 			solve(i+1, s[i+1][0] + sumN, s[i+1][1] + sumM);
+ 			ret = Math.max(ret, solve(capactiy + need[item], tonosi+happy[item], item+1));
 		}
 	
+		return ret;
 		
 	}
 		
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		
-		System.setIn(new FileInputStream("D://Project/Algorithm/Algorithm/BaekJooN/TestCase/Sample.txt"));
-		//System.setIn(new FileInputStream("C://Users//영훈//git//Algorithm//BaekJooN//TestCase//Sample.txt"));
+		//System.setIn(new FileInputStream("D://Project/Algorithm/Algorithm/BaekJooN/TestCase/Sample.txt"));
+		System.setIn(new FileInputStream("C://Users//영훈//git//Algorithm//BaekJooN//TestCase//Sample.txt"));
 		Scanner sc = new Scanner(System.in);
 		
 		
@@ -46,17 +54,23 @@ public class Main {
 		
 		k = sc.nextInt();
 		
-		s = new int[k+1][2];
+		cache = new int[1000][1000];
+		
+		happy = new int[1000];
+		need = new int[1000];
+				
+		
+		for(int[] t : cache) {
+			Arrays.fill(t, -1);
+		}
 		
 		for(int i=1; i<=k; i++) {
-			s[i][0] = sc.nextInt();
-			s[i][1] = sc.nextInt();
+			need[i] = sc.nextInt();
+			happy[i] = sc.nextInt();
 		}
 		
 		
-		solve(0, 0, 0);
-		
-		System.out.println(ret);
+		System.out.println(solve(0, 0, 1));
 	}
 		
 }
