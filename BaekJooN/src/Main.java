52f049fc100dd176b95ad13a8f8b22720b568c7c
starterfly <starterfly@gmail.com>
2018-10-09 17:11:23 +0900

    r

--- BaekJooN/src/Main.java
+++ BaekJooN/src/Main.java
@@ -1,153 +1,134 @@
 import java.io.FileInputStream;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
-class Dice {
-	
-	// 0 À­¸é, 1 ¾Æ·§¸é, 2 µ¿ÂÊ, 3¼­ÂÊ, 4 ºÏÂÊ, 5³²ÂÊ
-	int[] m = new int[6];
-	
-	void move(int s) {
-		
-		int t[] = new int[6];
-		int idx = 0;
-		for (int tmp : m) {
-			t[idx++] = tmp;
-		}
-		
-		switch(s) {
-			case 1:
-				m[0] = t[3];
-				m[1] = t[2];
-				m[2] = t[0];
-				m[3] = t[1];
-				break;
-			case 2:
-				m[0] = t[2];
-				m[1] = t[3];
-				m[2] = t[1];
-				m[3] = t[0];
-				break;
-			case 3:
-				// 0 À­¸é, 1 ¾Æ·§¸é, 2 µ¿ÂÊ, 3¼­ÂÊ, 4 ºÏÂÊ, 5³²ÂÊ
-				m[0] = t[5];
-				m[1] = t[4]; 
-				m[4] = t[0];
-				m[5] = t[1];
-				break;
-			case 4:
-				m[0] = t[4];
-				m[1] = t[5];
-				m[4] = t[1];
-				m[5] = t[0];
-				break;
-		}
-	}
-	
-	void copyNumber(int c) {
-		m[1] = c;
-	}
-	
-	int bottom() {
-		return m[1];
-	}
-	
-
-	int top() {
-		return m[0];
-	}
-}
-
 public class Main {
-	 
+	static int[][] map;
 	static int n;
 	static int m;
+	static int max;
 	
-	static int[][] map;
-	static int[] order;
+	static int X = 0;
+	static int Y = 1;
 	
-	static int[] X = {0, 1, -1, 0, 0};
-	static int[] Y = {0, 0, 0, -1, 1};
+	static int[] NX = {-1, 1, 0, 0};
+	static int[] NY = {0, 0, -1, 1};
 	
-	static void solve(int x, int y) {
-		// µ¿ÂÊÀº 1, ¼­ÂÊÀº 2, ºÏÂÊÀº 3, ³²ÂÊÀº 4·Î ÁÖ¾îÁø´Ù.
-		Dice dice = new Dice();
+	static Queue<int[]> q;
+	
+	static void bfs() {
 		
-		/* 
-		 *  4 2 0 0 8
-			0 2
-			3 4
-			5 6
-			7 8
-			4 4 4 1 3 3 3 2
-		 */
-		int nowX = x;
-		int nowY = y;
+		Queue<int[]> _q = new LinkedList<int[]>(q);
 		
-		for(int i=0; i<order.length; i++) {
-			int nx = nowX + X[order[i]]; 
-			int ny = nowY + Y[order[i]];
-			if(map[ny][nx] == -1 ) {
-				continue;
-			}else if(map[ny][nx] == 0 ) {
-				dice.move(order[i]);
+		
+		while(!_q.isEmpty()) {
+			
+			int[] t = _q.poll();
+			
+			for(int i=0; i<4;i++) {
+				int _nx = t[X] + NX[i];
+				int _ny = t[Y] + NY[i];
 				
-				nowX = nx; 
-				nowY = ny;
-				
-				map[ny][nx] = dice.bottom();
-				
-				System.out.println(dice.top());
-			}else {
-				dice.move(order[i]);
-				
-				nowX = nx; 
-				nowY = ny;
-				
-				dice.copyNumber(map[ny][nx]);
-				map[ny][nx] = 0;
-				
-				System.out.println(dice.top());
+				if(map[_ny][_nx] == 0) {
+					map[_ny][_nx] = 2;
+					
+					int s[] = new int[2];
+					s[X] = _nx;
+					s[Y] = _ny;
+					_q.add(s);
+				}
+			}
+		}
+	}
+	
+	static int cnt() {
+		int ret=0;
+		
+		bfs();
+		
+		for(int[] t : map) {
+			for(int c : t) {
+				if(c==0) {
+					ret++;
+				}
 			}
 		}
 		
+		return ret;
+	}
+	
+	static void dfs(int firewall) {
+		
+		if(firewall > 3) {
+			int ret = cnt();
+			if(max < ret) {
+				max = ret;
+			}
+			
+			return;
+		}
+		
+		int[][] tmap = new int[n+2][m+2];
+		for(int i=1; i<=n;i++) {
+			for(int j=1; j<=m; j++) {
+				if(map[i][j] == 0) {
+					
+					int idx=0;
+					for(int[] t : map) {
+						System.arraycopy(t, 0, tmap[idx++], 0, m+2);
+					}
+					
+					map[i][j] = 7;
+					dfs(firewall + 1);
+					
+					idx=0;
+					for(int[] t : tmap) {
+						System.arraycopy(t, 0, map[idx++], 0, m+2);
+					}
+				}
+			}
+		}
 	}
 	
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		
 		//System.setIn(new FileInputStream("D://Project/Algorithm/Algorithm/BaekJooN/TestCase/Sample.txt"));
-		System.setIn(new FileInputStream("C://Algorithm/workspace/Algorithm/BaekJooN/TestCase/Sample.txt"));
+		//System.setIn(new FileInputStream("C://Algorithm/workspace/Algorithm/BaekJooN/TestCase/Sample.txt"));
+		System.setIn(new FileInputStream("C://Algorithm/Test/Algorithm/BaekJooN/TestCase/Sample.txt"));
 		Scanner sc = new Scanner(System.in);
 		
 		n = sc.nextInt();
 		m = sc.nextInt();
 		
-		int sy = sc.nextInt();
-		int sx = sc.nextInt();
-		
-		int s = sc.nextInt();
-		
-		order = new int[s];
-		
 		map = new int[n+2][m+2];
 		
-		for(int[] t : map) {
+		q = new LinkedList<int[]>();
+		
+		for(int t[] : map) {
 			Arrays.fill(t, -1);
 		}
 		
 		for(int i=1; i<=n;i++) {
-			for(int j=1; j<=m;j++) {
-				map[i][j] = sc.nextInt();
+			for(int j=1; j<=m; j++) {
+				int t = sc.nextInt();
+				
+				map[i][j] = t;
+				
+				if(t==2) {
+					int[] tq = new int[2];
+					tq[X] = j;
+					tq[Y] = i;
+					
+					q.add(tq);
+				}
 			}
 		}
 		
-		for(int i=0; i<s; i++) {
-			order[i] = sc.nextInt();
-		}
+		dfs(1);
 		
-		solve(sx+1, sy+1);
+		System.out.println(max);
 	}
-	
 }
 
