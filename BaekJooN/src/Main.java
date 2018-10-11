fc84807a9141ba25e2af2d48677d839f888a393b
godokan <godokan@DESKTOP-UMPDF02.mshome.net>
2018-10-11 18:26:53 +0900

    r

--- BaekJooN/src/Main.java
+++ BaekJooN/src/Main.java
@@ -1,79 +1,58 @@
 import java.io.FileInputStream;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.Vector;
 
 public class Main {
 	
 	static int n;
 	static int m;
-	static int[][] map;
-	static int[][] visited;
-	static int[] X = {-1, 1, 0, 0};
-	static int[] Y = {0, 0, -1, 1};
-	static int max = 0;
+	static int s;
 	
-	static int _X = 0;
-	static int _Y = 1;
+	static int day = 0;
+	static int happy = 1;
 	
-	static void bfs(int i, int j) {
-			int min = Integer.MAX_VALUE;
-			int sum = map[i][j];
-			int block = 0;
-			
-			// 십자가 모양으로 찾는다.
-			for(int k=0; k<4; k++) {
-				
-				int ny = i + Y[k];  
-				int nx = j + X[k]; 
-				
-				
-				if(map[ny][nx] != -1) {
-					sum+=map[ny][nx];
-					
-					min = Math.min(min, map[ny][nx]);
-					block++;
-				}
-			}
-			
-			if(block == 4) {
-				sum-=min;
-			}
-			
-			max = Math.max(sum, max);
-	}
+	static Vector<int[]> vc;
 	
-	static void dfs(int sum, int c, int _x, int _y) {
+	static int max;
+	
+	static int[] visited;
+	
+	static void solve(int sum, int day, int c) {
 		
-		if(c == 4) {
-				max = Math.max(sum, max);
+		if(s==c) {
+			max = Math.max(max, sum);
 			return;
 		}
 		
-		for(int k=0; k<4; k++) {
+		solve(0, 0, c+1);
+		
+		for(int i=0;i<s;i++) {
+			int t[] = vc.get(i);
 			
-			int nx = _x + X[k];
-			int ny = _y + Y[k];
-			
-			if(map[ny][nx] != -1 && visited[ny][nx] != 1) {
-				
-				visited[ny][nx] = 1;
-				
-				dfs(sum + map[ny][nx], c+1, nx, ny);
-				
-				visited[ny][nx] = 0;
-				
+			if(visited[i] == 0) {
+				visited[i] = 1;
+				solve(sum +  t[happy], day + t[day], i);
+				visited[i] = 0;
 			}
 		}
+		
 	}
 	
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 
 		//회사PC
-		//System.setIn(new FileInputStream("D://Project/Algorithm/Algorithm/BaekJooN/TestCase/Sample.txt"));
+		System.setIn(new FileInputStream("D://Project/Algorithm/Algorithm/BaekJooN/TestCase/Sample.txt"));
 		
 		//데스크탑
-		System.setIn(new FileInputStream("C://Algorithm/workspace/Algorithm/BaekJooN/TestCase/Sample.txt"));
+		//System.setIn(new FileInputStream("C://Algorithm/workspace/Algorithm/BaekJooN/TestCase/Sample.txt"));
 		
 		//노트북
 		//System.setIn(new FileInputStream("C://Algorithm/Test/Algorithm/BaekJooN/TestCase/Sample.txt"));
@@ -81,34 +60,21 @@ public class Main {
 		
 		n = sc.nextInt();
 		m = sc.nextInt();
+		s = sc.nextInt();
 		
-		map = new int[n+2][m+2];
-		visited = new int[n+2][m+2];
+		vc = new Vector<int[]>();
+		visited = new int[m];
 		
-		for(int t[] : map) {
-			Arrays.fill(t, -1);
+		for(int i=0; i<s;i++) {
+			int[] t = new int[2];
+			t[day] = sc.nextInt();
+			t[happy] = sc.nextInt();
+			
+			vc.add(t);
 		}
 		
-		for(int i=1; i<=n; i++) {
-			for(int j=1; j<=m; j++) {
-				map[i][j] = sc.nextInt();
-			}
-		}
+		solve(0, 0, 0);
 		
-		for(int i=1; i<=n; i++) {
-			for(int j=1; j<=m; j++) {
-				visited[i][j] = 1;
-				dfs(map[i][j], 1, j, i);
-				bfs(i, j);
-				visited[i][j] = 0;
-				
-				
-			}
-		}
-		
-		
-		
-		System.out.println(max);
 	}
 }
 
