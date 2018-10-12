82995f6d6de52ebdd910e1ed3e7f39c0b84d3a04
starterfly <starterfly@gmail.com>
2018-10-12 13:41:12 +0900

    r

--- BaekJooN/src/Main.java
+++ BaekJooN/src/Main.java
@@ -1,13 +1,6 @@
 import java.io.FileInputStream;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.Vector;
 
 public class Main {
 	
@@ -15,33 +8,16 @@ public class Main {
 	static int m;
 	static int s;
 	
-	static int day = 0;
-	static int happy = 1;
-	
-	static Vector<int[]> vc;
-	
 	static int max;
 	
+	static int[] dp;
 	static int[] visited;
 	
-	static void solve(int sum, int day, int c) {
+	static int happy[];
+	static int score[];
+
+	static void solve() {
 		
-		if(s==c) {
-			max = Math.max(max, sum);
-			return;
-		}
-		
-		solve(0, 0, c+1);
-		
-		for(int i=0;i<s;i++) {
-			int t[] = vc.get(i);
-			
-			if(visited[i] == 0) {
-				visited[i] = 1;
-				solve(sum +  t[happy], day + t[day], i);
-				visited[i] = 0;
-			}
-		}
 		
 	}
 	
@@ -49,31 +25,48 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		//회사PC
-		System.setIn(new FileInputStream("D://Project/Algorithm/Algorithm/BaekJooN/TestCase/Sample.txt"));
+		//System.setIn(new FileInputStream("D://Project/Algorithm/Algorithm/BaekJooN/TestCase/Sample.txt"));
 		
 		//데스크탑
 		//System.setIn(new FileInputStream("C://Algorithm/workspace/Algorithm/BaekJooN/TestCase/Sample.txt"));
 		
 		//노트북
-		//System.setIn(new FileInputStream("C://Algorithm/Test/Algorithm/BaekJooN/TestCase/Sample.txt"));
+		System.setIn(new FileInputStream("C://Algorithm/Test/Algorithm/BaekJooN/TestCase/Sample.txt"));
 		Scanner sc = new Scanner(System.in);
 		
 		n = sc.nextInt();
 		m = sc.nextInt();
 		s = sc.nextInt();
 		
-		vc = new Vector<int[]>();
-		visited = new int[m];
+		score = new int[s];
+		happy = new int[s];
+		
+		dp = new int[s+1];
+		
+		//Arrays.fill(dp, Integer.MIN_VALUE);
 		
 		for(int i=0; i<s;i++) {
 			int[] t = new int[2];
-			t[day] = sc.nextInt();
-			t[happy] = sc.nextInt();
 			
-			vc.add(t);
+			t[0] = sc.nextInt();
+			t[1] = sc.nextInt();
+			
+			if(t[0]==0) {
+				if(t[1] > 0) {
+					dp[t[0]] += t[1];
+				}
+			}else {
+				score[i] = t[0];
+				happy[i] = t[1];
+			}
+			max = Math.max(score[i], max);
 		}
 		
-		solve(0, 0, 0);
+		visited = new int[max+1];
+		
+		solve();
+		
+		System.out.print(max);
 		
 	}
 }
