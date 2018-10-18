be89ce61d791bbf4fd8d8bffce91fea8416e4c04
starterfly <starterfly@gmail.com>
2018-10-19 00:53:28 +0900

    r

--- BaekJooN/src/Main.java
+++ BaekJooN/src/Main.java
@@ -2,103 +2,51 @@ import java.io.FileInputStream;
 import java.util.Scanner;
 
 public class Main {
-
-	static int n;
-	static int m;
-	
-	static int c;
-	
-	static int[] score;
-	static int[] statis;
-	
-	static int[] visited;
-	
-	static int ret;
-	static int retScore;
-	
-	static int[] dp;
-	
-
-	/*
-	static void dfs(int _idx, int _score, int _statis) {
-
-		if(_idx == c) {
-			
-			if(_score>=n) {
-				ret = Math.max(ret, _statis);
-			}
-			
-			return;
-		}
-		
-		dfs(_idx+1, _score, _statis);
-		
-		if(_score + score[_idx] <=m) {
-			dfs(_idx+1, _score + score[_idx], _statis+statis[_idx]);
-		}
-		
-	}
-	*/
-	
-	static void dp() {
-		
-		//1 2 3
-		/*
-			
-		 */
-		
-		
-		for(int i=1; i<c;i++) {
-			for()
-			
-			
-		}
-		
-	}
-	
-	
-	
+	static int[] w;
+	static int[] v;
+	static int[][] d;
 	
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 
 		//회사PC
-		System.setIn(new FileInputStream("D://Project/Algorithm/Algorithm/BaekJooN/TestCase/Sample.txt"));
+		//System.setIn(new FileInputStream("D://Project/Algorithm/Algorithm/BaekJooN/TestCase/Sample.txt"));
 		
 		//데스크탑
 		//System.setIn(new FileInputStream("C://Algorithm/workspace/Algorithm/BaekJooN/TestCase/Sample.txt"));
 		
 		//노트북
-		//System.setIn(new FileInputStream("C://Algorithm/Test/Algorithm/BaekJooN/TestCase/Sample.txt"));
+		System.setIn(new FileInputStream("C://Algorithm/Test/Algorithm/BaekJooN/TestCase/Sample.txt"));
 		
 		Scanner sc = new Scanner(System.in);
 		
-		n = sc.nextInt();
-		m = sc.nextInt();
-		c = sc.nextInt();
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		//int C = sc.nextInt();
 		
-		score = new int[c];
-		statis = new int[c];
-		
-		dp = new int[c];
-		
-		for(int i=0;i<c;i++) {
-			score[i] = sc.nextInt();
-			statis[i] = sc.nextInt();
-			
-			if(score[i] == 0 && statis[i] > 0) {
-				dp[0] +=  statis[i];
-			}
-			
-		}
-		
-		//dfs(0, 0, 0);
-		
-		dp();
-		
-		System.out.println(ret);
-		
-	}
-	
+        //N+10 을 해준 이유는 마지막날 + 5일일 때 배열 에러가 날 수 있으므로 넉넉히 잡아준다. 
+        w = new int[N+10];
+        v = new int[N+10];
+        d = new int[K+1][K+1];
+        
+        int max = 0;
+        for (int i = 1; i <= N; i++) {
+            w[i] = sc.nextInt();
+            v[i] = sc.nextInt();
+        }
+        
+        int free_space;
+        for (int i = 1; i <= N; i++) {
+            for(int j = 1; j <= K; j++) {
+                if(w[i] <= j){
+                    free_space = j-w[i];
+                    d[i][j] = Math.max(d[i-1][j], v[i]+d[i-1][free_space]);
+                }else{
+                    d[i][j] = d[i-1][j];
+                }
+            }
+        }
+        System.out.println(d[N][K]);
+    }
 }
 
