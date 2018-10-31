83a221a76081572879fb5cd24fc2ef7b3e299f9b
godokan <godokan@DESKTOP-UMPDF02>
2018-10-31 13:20:46 +0900

    r

---
+++ BaekJooN/src/Setin.java
@@ -0,0 +1,15 @@
+import java.io.FileInputStream;
+
+public class Setin {
+	//회사PC
+	//System.setIn(new FileInputStream("D://Project/Algorithm/Algorithm/BaekJooN/TestCase/Sample.txt"));
+	
+	//데스크탑
+	//System.setIn(new FileInputStream("C://Algorithm/workspace/Algorithm/BaekJooN/TestCase/Sample.txt"));
+	
+	//노트북
+	//System.setIn(new FileInputStream("C://Algorithm/Test/Algorithm/BaekJooN/TestCase/Sample.txt"));
+	
+	
+	
+}
