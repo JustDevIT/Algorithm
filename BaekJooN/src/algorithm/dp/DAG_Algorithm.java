24a198884ac17c6c3564c2ba252a7c6bdade2db0
godokan <godokan@DESKTOP-UMPDF02>
2018-10-31 13:21:27 +0900

    r

--- BaekJooN/src/algorithm/dp/DAG_Algorithm.java
+++ BaekJooN/src/algorithm/dp/DAG_Algorithm.java
@@ -6,48 +6,55 @@ import java.util.Scanner;
 
 public class DAG_Algorithm {
 	
-	static int n, m, k;
+	static int n;
+	static int m;
+	static int c;
+	
 	static int[][] ticket;
 	static int[][] cache;
 	
-	static int Search(int idx, int visit) {
+	
+	static int solve(int idx, int visit) {
 		
-		if(idx != n && visit == m) {
+		// base case
+		if(idx != n && visit ==m) {
 			return Integer.MIN_VALUE;
 		}
-		if(idx==n) {
+		
+		if(idx == n) {
 			return 0;
 		}
 		
-		if(cache[idx][visit] != -1) {
+		if(cache[idx][visit]>0) {
 			return cache[idx][visit];
 		}
 		
-		for(int to = idx + 1; to <=n; to++) {
-			System.out.println(to + " " + (visit+1));
+		
+		for(int to=idx+1; to<=n; to++) {
 			if(ticket[idx][to] > 0) {
-				cache[idx][visit]  = Math.max(cache[idx][visit], ticket[idx][to] + Search(to, visit + 1));
+				cache[idx][visit] = Math.max(cache[idx][visit], ticket[idx][to] + solve(to, visit + 1));
 			}
 		}
 		
-		return cache[idx][visit] ;
+		
+		return cache[idx][visit];
+		
 	}
 	
+
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-
-		//데스크탑
-		System.setIn(new FileInputStream("C://Algorithm/workspace/Algorithm/BaekJooN/TestCase/Sample.txt"));
-
+		
+		System.setIn(new FileInputStream("D://Project/Algorithm/Algorithm/BaekJooN/TestCase/Sample.txt"));
+		
 		Scanner sc = new Scanner(System.in);
 		
 		n = sc.nextInt();
 		m = sc.nextInt();
-		k = sc.nextInt();
+		c = sc.nextInt();
 		
-		ticket = new int[k+1][k+1];
-		cache = new int[k+1][k+1];
-		
+		ticket = new int[301][301];
+		cache = new int[301][301];
 		
 		for(int[] t : ticket) {
 			Arrays.fill(t, -1);
@@ -57,16 +64,16 @@ public class DAG_Algorithm {
 			Arrays.fill(t, -1);
 		}
 		
-		for(int i =0; i < k; i++) {
-			int city1, city2, taste;
-			
-			city1 = sc.nextInt();
-			city2 = sc.nextInt();
-			taste = sc.nextInt();
+		for(int i=0; i<c; i++) {
+			int city1 = sc.nextInt();
+			int city2 = sc.nextInt();
+			int taste = sc.nextInt();
 			
 			ticket[city1][city2] = Math.max(ticket[city1][city2], taste);
-		}
+		} 
 		
-		System.out.println(Search(1, 1));
+		System.out.println(solve(1,1));
+		
 	}
+	
 }
